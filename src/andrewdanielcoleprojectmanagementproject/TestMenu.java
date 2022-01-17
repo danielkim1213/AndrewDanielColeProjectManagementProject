56d86512a1d351b27b7ee177ae48c5a01569645f
Daniel Kim <kyw4091@gmail.com>
2022-01-17 11:50:33 -0500

    new commit

--- src/andrewdanielcoleprojectmanagementproject/TestMenu.java
+++ src/andrewdanielcoleprojectmanagementproject/TestMenu.java
@@ -5,18 +5,73 @@
  */
 package andrewdanielcoleprojectmanagementproject;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author colem
  */
 public class TestMenu extends javax.swing.JFrame {
     MainMenu mainMenu;
+    
+    private void scanFile()
+    {
+        Quiz[] qz = new Quiz[10];
+        try{
+            File file = new File("src/andrewdanielcoleprojectmanagementproject/questions & answers.txt");
+            Scanner scan = new Scanner(file);
+            
+            String question;
+            int answer;
+            String ansDescription;
+            String[] answerOptions = new String[4];
+            int numQuestion;
+            
+            for(int n=0; n<10; n++)
+            {
+                question = scan.nextLine();
+                for(int i=0; i<4; i++)
+                {
+                    answerOptions[i] = scan.nextLine();
+                }
+                answer = Integer.parseInt(scan.nextLine());
+                ansDescription = scan.nextLine();
+                
+                numQuestion = n+1;
+                
+                qz[n] = new Quiz(numQuestion, question, answerOptions, answer, ansDescription);
+            }
+        }catch(FileNotFoundException e)
+        {
+            System.out.print(e);
+        }
+        display(qz[0]);
+    }
+    
+    private void display(Quiz qz) 
+    {
+        int numQ = qz.getNumQuestion();
+        btnQuestion.setText(qz.getQuestion());
+        radOptionOne.setText(qz.getAnswerOption(1));
+        radOptionTwo.setText(qz.getAnswerOption(2));
+        radOptionThree.setText(qz.getAnswerOption(3));
+        radOptionFour.setText(qz.getAnswerOption(4));
+        prgNumQuestion.setValue((numQ*10));
+        lblNumQuestion.setText("Question: " + numQ + "/10");
+        
+        if(numQ == 10)
+        {
+            btnNext.setText("Done");
+        }
+    }
     /**
      * Creates new form TestMenu
      */
     public TestMenu(MainMenu m) {
         initComponents();
         mainMenu = m;
+        scanFile();
     }
 
     /**
@@ -35,7 +90,7 @@ public class TestMenu extends javax.swing.JFrame {
         radOptionTwo = new javax.swing.JRadioButton();
         radOptionThree = new javax.swing.JRadioButton();
         radOptionFour = new javax.swing.JRadioButton();
-        jProgressBar1 = new javax.swing.JProgressBar();
+        prgNumQuestion = new javax.swing.JProgressBar();
         lblNumQuestion = new javax.swing.JLabel();
         btnNext = new javax.swing.JButton();
 
@@ -53,20 +108,25 @@ public class TestMenu extends javax.swing.JFrame {
         btnQuestion.setText("Question");
 
         btngroupAnswerOptions.add(radOptionOne);
+        radOptionOne.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
         radOptionOne.setText("option1");
 
         btngroupAnswerOptions.add(radOptionTwo);
+        radOptionTwo.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
         radOptionTwo.setText("option2");
 
         btngroupAnswerOptions.add(radOptionThree);
+        radOptionThree.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
         radOptionThree.setText("option3");
 
         btngroupAnswerOptions.add(radOptionFour);
+        radOptionFour.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
         radOptionFour.setText("option4");
 
         lblNumQuestion.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         lblNumQuestion.setText("Question: 1/10");
 
+        btnNext.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         btnNext.setText("Next");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -87,7 +147,7 @@ public class TestMenu extends javax.swing.JFrame {
                                     .addComponent(radOptionOne))
                                 .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 472, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(prgNumQuestion, javax.swing.GroupLayout.PREFERRED_SIZE, 472, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(lblNumQuestion, javax.swing.GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)))
                         .addContainerGap())
@@ -102,7 +162,7 @@ public class TestMenu extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(btnQuestion, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(18, 18, 18)
                 .addComponent(radOptionOne)
                 .addGap(18, 18, 18)
                 .addComponent(radOptionTwo)
@@ -110,15 +170,15 @@ public class TestMenu extends javax.swing.JFrame {
                 .addComponent(radOptionThree)
                 .addGap(18, 18, 18)
                 .addComponent(radOptionFour)
-                .addGap(49, 49, 49)
+                .addGap(35, 35, 35)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnMainMenu)
                     .addComponent(btnNext))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(lblNumQuestion, javax.swing.GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE)
-                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(30, Short.MAX_VALUE))
+                    .addComponent(prgNumQuestion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
 
         pack();
@@ -139,8 +199,8 @@ public class TestMenu extends javax.swing.JFrame {
     private javax.swing.JButton btnNext;
     private javax.swing.JLabel btnQuestion;
     private javax.swing.ButtonGroup btngroupAnswerOptions;
-    private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JLabel lblNumQuestion;
+    private javax.swing.JProgressBar prgNumQuestion;
     private javax.swing.JRadioButton radOptionFour;
     private javax.swing.JRadioButton radOptionOne;
     private javax.swing.JRadioButton radOptionThree;
