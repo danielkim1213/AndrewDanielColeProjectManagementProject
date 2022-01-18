bfff783f0d771870cb17914dfc43c1cdb2fc1f67
Andrew <97902041+andrewfehr@users.noreply.github.com>
2022-01-17 19:10:35 -0500

    Comment StudyMenu and fix file loading for JAR file

--- src/andrewdanielcoleprojectmanagementproject/TestMenu.java
+++ src/andrewdanielcoleprojectmanagementproject/TestMenu.java
@@ -7,6 +7,7 @@ package andrewdanielcoleprojectmanagementproject;
 
 import java.io.*;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -24,7 +25,7 @@ public class TestMenu extends javax.swing.JFrame {
     private void scanFile()
     {
         try{
-            File file = new File("src/andrewdanielcoleprojectmanagementproject/questions & answers.txt"); //find the file
+            InputStream file = StudyMenu.class.getResourceAsStream("questions & answers.txt"); //find the file
             Scanner scan = new Scanner(file); //Scanner object that will scan the file
             
             String question; 
@@ -42,9 +43,9 @@ public class TestMenu extends javax.swing.JFrame {
                 
                 qz[n] = new Quiz(n+1, question, answerOptions.clone(), answer); //instantiate new Quiz object and save it in qz array.
             }
-        }catch(FileNotFoundException e) //if the program cannot find the file
+        }catch(Exception e) //if the program cannot find the file
         {
-            System.out.print(e); //print the error
+            JOptionPane.showMessageDialog(null, "Error: quiz questions file not found"); //print the error
         }
     }
     
